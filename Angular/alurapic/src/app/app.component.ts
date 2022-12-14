656e23efc4313504db9562939b2dabb062f9ce01
Henrique PD <henrique@pdsolucoes.com.br>
2022-12-14 11:54:33 -0300

    feat: add new component to show an image

--- Angular/alurapic/src/app/app.component.ts
+++ Angular/alurapic/src/app/app.component.ts
@@ -6,9 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
-  title = 'Jotaro';
-  description = 'Jotaro';
-  url = 'http://pm1.narvii.com/8134/05481da521602f31f89f942eed3e6988263dda3cr1-500-500v2_uhq.jpg';
-
 }
---
+++ Angular/alurapic/src/app/photo/photo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'ap-photo',
+    templateUrl: 'PhotoComponent.html'
+})
+export class PhotoComponent {
+    description = 'Jotaro';
+    url = 'http://pm1.narvii.com/8134/05481da521602f31f89f942eed3e6988263dda3cr1-500-500v2_uhq.jpg';
+}
