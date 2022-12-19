798e54f3902bce590a3bc6cb5eb4d4c07e24ecf4
HenriqueMAlves <m.henriquealves@gmail.com>
2022-12-19 20:13:20 -0300

    refactor: changes to modularize code

---
+++ Angular-charts/intelbras-test/src/app/dash/data.ts
@@ -0,0 +1,84 @@
+export var data: any[] = [
+    {
+      name: '01/11',
+      series: [
+        {
+          name: 'permitidos',
+          value: 60
+        },
+        {
+          name: 'negados',
+          value: 10
+        },
+        {
+          name: 'alarmes',
+          value: 0
+        }
+      ]
+    },
+    {
+      name: '02/11',
+      series: [
+        {
+          name: 'permitidos',
+          value: 60
+        },
+        {
+          name: 'negados',
+          value: 5
+        },
+        {
+          name: 'alarmes',
+          value: 1
+        }
+      ]
+    },    {
+      name: '03/11',
+      series: [
+        {
+          name: 'permitidos',
+          value: 30
+        },
+        {
+          name: 'negados',
+          value: 0
+        },
+        {
+          name: 'alarmes',
+          value: 2
+        }
+      ]
+    },    {
+      name: '04/11',
+      series: [
+        {
+          name: 'permitidos',
+          value: 20
+        },
+        {
+          name: 'negados',
+          value: 10
+        },
+        {
+          name: 'alarmes',
+          value: 0
+        }
+      ]
+    },    {
+      name: '05/11',
+      series: [
+        {
+          name: 'permitidos',
+          value: 90
+        },
+        {
+          name: 'negados',
+          value: 30
+        },
+        {
+          name: 'alarmes',
+          value: 2
+        }
+      ]
+    }
+  ];
